1748a15de783580215ae57b29b7071dd137b206f
cyanoflame <cyanoflame@gmail.com>
2025-05-24 13:22:12 -0500

    created queries for creates and reads for sqlite

--- src/database/sqlite/SqliteDb.ts
+++ src/database/sqlite/SqliteDb.ts
@@ -1,15 +1,17 @@
 import { Database, SQLiteError } from "bun:sqlite"
 
+/** This is used throughout the class for when the  */
+const dbNotInitializedError: ReferenceError = ReferenceError("Database has not been initialized"); // TODO: Language support for this
 
 /**
- * This is used for accessing an Sqlite database to retrieve data.
+ * This class is used for creating, managing, and accessing data from an Sqlite database.
  */
 export default class SqliteDb {
     /** The singleton instance of the class */
     private static instance: SqliteDb | undefined;
 
     /** The reference to the database */
-    private static db: Database | undefined;
+    private static db: Database | undefined = undefined;
 
     /**
      * The constructor for the database object. If the DB has not been created, it will be 
@@ -28,7 +30,7 @@ export default class SqliteDb {
             if(err instanceof SQLiteError && err.errno == 21) {
                 // Create the new DB
                 SqliteDb.db = new Database(path, {
-                    create: false,
+                    create: true,
                     strict: true
                 })
 
@@ -38,48 +40,198 @@ export default class SqliteDb {
         }
     }
 
-    public static getInstance(path: string = ":memory:"): SqliteDb {
+    /**
+     * This method is used to initialize the database instance. It must be run first before the 
+     * class can be used anywhere.
+     * @param path The path to the sqlite db file, or, leave blank or use :memory: to just keep in memory.
+     */
+    public initialize(path: string = ":memory:"): void {
         // if there is no DB
         if(SqliteDb.instance == undefined) {
             // create the DB
             SqliteDb.instance = new SqliteDb(path);
         }
+    }
+
+    /**
+     * This gets the current 
+     * @returns The current sqlite db being used.
+     * @throws ReferenceError if the DB is not initialized yet.
+     */
+    public static getInstance(): SqliteDb {
+        // if there is no DB
+        if(SqliteDb.instance == undefined) {
+            // Throw an error because the db is not created yet
+            throw dbNotInitializedError;
+        }
         // Return the singleton instance
         return SqliteDb.instance;
     }
 
+    /**
+     * This private method is used to setup the schema for the database. It is used when 
+     * creating a new database.
+     * @param db The database which to setup the schema for.
+     */
     private createDatabaseSchema(db: Database): void {
         // enable write-ahead logging mode
         db.exec("PRAGMA journal_mode = WAL;");
 
-        // // create the server table
+        // enable foreign keys
+        db.exec("PRAGMA foreign_keys = 1;");
+
+        // create the guild table
+        db.run(`
+            CREATE TABLE guild_config (
+                id INTEGER NOT NULL PRIMARY KEY,
+                discord_guild_id INTEGER NOT NULL
+            );
+        `);
+
+        // // create the channel table
         // db.run(`
-        //     CREATE TABLE guild_config (
+        //     CREATE TABLE channel_config (
         //         id INTEGER NOT NULL PRIMARY KEY,
-        //         guild_id INTEGER NOT NULL
+        //         guild_id INT NOT NULL,
+        //         discord_channel_id INTEGER NOT NULL UNIQUE,
+        //         whitelisted INT NOT NULL,
+        //         blacklisted INT NOT NULL,
+        //         -- outputChannelId
+        //         -- ...
+        //         FOREIGN KEY(guild_id) REFERENCES guild_config(id)
         //     );
         // `);
 
-        // create the channel whitelist for guilds
+        // create the channel table
         db.run(`
             CREATE TABLE whitelisted_channel (
                 id INTEGER NOT NULL PRIMARY KEY,
                 guild_id INT NOT NULL,
-                channel_id INTEGER NOT NULL UNIQUE
+                discord_channel_id INTEGER NOT NULL UNIQUE,
+                FOREIGN KEY(guild_id) REFERENCES guild_config(id)
             );
         `);
 
-        // create the channel blacklist for guilds
+        // create the channel blacklist table
         db.run(`
             CREATE TABLE blacklisted_channel (
                 id INTEGER NOT NULL PRIMARY KEY,
                 guild_id INTEGER NOT NULL,
-                channel_id INTEGER NOT NULL UNIQUE
+                discord_channel_id INTEGER NOT NULL UNIQUE,
+                FOREIGN KEY(guild_id) REFERENCES guild_config(id)
             );
         `);
     }
 
-    public getWhitelist(guildId: number, channelId: number) {
-        db.query(`SELECT channel_id FROM whitelisted_channel WHERE guild_id = $guildId;`)
+    ///// C /////
+    
+    /**
+     * This is used to add a whitelisted channel.
+     * @param guildId the discord id of the guild to add a whitelisted channel for.
+     * @param channelId the discord id of the channel being whitelisted.
+     * @throws ReferenceError if the DB is not initialized yet.
+     */
+    public addToGuildWhitelist(guildId: number, channelId: number): void {
+        if(SqliteDb.db) {
+            // Insert new guild entry if needed
+            SqliteDb.db.query(`
+                INSERT OR IGNORE INTO guild_config (discord_guild_id) VALUES ($guildId);
+            `)
+            .run({
+                guildId: guildId
+            })
+
+            // Add a new whitelisted channel
+            SqliteDb.db.query(`
+                INSERT INTO whitelisted_channel (guild_id, discord_channel_id) 
+                    SELECT guild_config.id, $channelId FROM guild_config WHERE discord_guild_id = $guildId;
+            `)
+            .run({
+                guildId: guildId,
+                channelId: channelId
+            });
+        } else {
+            throw dbNotInitializedError;
+        }
     }
+
+    /**
+     * This is used to add a whitelisted channel.
+     * @param guildId the discord id of the guild to add a whitelisted channel for.
+     * @param channelId the discord id of the channel being whitelisted.
+     * @throws ReferenceError if the DB is not initialized yet.
+     */
+    public addToGuildBlacklist(guildId: number, channelId: number): void {
+        if(SqliteDb.db) {
+            // Insert new guild entry if needed
+            SqliteDb.db.query(`
+                INSERT OR IGNORE INTO guild_config (discord_guild_id) VALUES ($guildId);
+            `)
+            .run({
+                guildId: guildId
+            })
+
+            // Add a new whitelisted channel
+            SqliteDb.db.query(`
+                INSERT INTO blacklisted_channel (guild_id, discord_channel_id) 
+                    SELECT guild_config.id, $channelId FROM guild_config WHERE discord_guild_id = $guildId;
+            `)
+            .run({
+                guildId: guildId,
+                channelId: channelId
+            });
+        } else {
+            throw dbNotInitializedError;
+        }
+    }
+
+    ///// R /////
+
+    /**
+     * This is used to get the whitelisted channels for a guild.
+     * @param guildId the id of the discord guild to get the whitelisted channels for.
+     * @throws ReferenceError if the DB is not initialized yet.
+     */
+    public getGuildWhitelist(guildId: number): {channelId: number}[] {
+        if(SqliteDb.db) {
+            return SqliteDb.db.query<{channelId: number}, {guildId: number}>(`
+                SELECT whitelisted_channel.discord_channel_id FROM whitelisted_channel 
+                    JOIN guild_config ON whitelisted_channel.guild_id = guild_config.id 
+                    WHERE guild_config.discord_guild_id = $guildId;
+            `)
+            .all({
+                guildId: guildId
+            });
+        } else {
+            throw dbNotInitializedError;
+        }
+    }
+
+    /**
+     * This is used to get the blacklisted channels for a guild.
+     * @param guildId the id of the discord guild to get the blacklisted channels for.
+     * @throws ReferenceError if the DB is not initialized yet.
+     */
+    public getGuildBlacklist(guildId: number): {channelId: number}[] {
+        if(SqliteDb.db) {
+            return SqliteDb.db.query<{channelId: number}, {guildId: number}>(`
+                SELECT blacklisted_channel.discord_channel_id FROM blacklisted_channel 
+                    JOIN guild_config ON blacklisted_channel.guild_id = guild_config.id 
+                    WHERE guild_config.discord_guild_id = $guildId;
+            `)
+            .all({
+                guildId: guildId
+            });
+        } else {
+            throw dbNotInitializedError;
+        }
+    }
+
+    ///// U /////
+
+    // None
+
+    ///// D /////
+
+    //
 }
